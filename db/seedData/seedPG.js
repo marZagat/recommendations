fd5fb985239679daf8e4ebe63415d5689fd4ce96
Annisa <annisakaraca@gmail.com>
2018-03-17 15:34:25 -0700

    Add review generation and insertion to seed script

--- db/seedData/seedPG.js
+++ db/seedData/seedPG.js
@@ -45,7 +45,7 @@ function generateRestaurantDataBatch(round) {
       longitude: faker.address.longitude(),
       address: faker.address.streetAddress('###'),
       website: faker.internet.url(),
-      price_level: faker.random.number(3),
+      price_level: faker.random.number(3) + 1,
       type: placeTypes[faker.random.number(5)]
     }
     data.push(restaurantRow);
@@ -53,7 +53,7 @@ function generateRestaurantDataBatch(round) {
   return Promise.resolve(data);
 }
 
-db.tx('inserting-products', t => {
+db.tx('inserting-reviews', t => {
   return t.sequence(index => {
     return generateRestaurantDataBatch(index)
     .then(data => {
@@ -65,34 +65,49 @@ db.tx('inserting-products', t => {
   })
 })
   .then((data) => {
-    console.log('inserted batch');
+    console.log('inserted restaurants');
   })
   .catch(error => {
     console.log(error);
   })
 
-  function generateReviewDataBatch(round) {
-    console.log(round);
-    if (round > 25000) {
-      return Promise.resolve(null);
-    }
-    let data = [];
-    for (var x = 0; x < 400; x++) {
-      let restaurantRow = {
-        review_id: (400*round) + x,
-        restaurant_id: (400*round) + x
-        name: faker.company.companyName(), 
-        neighborhood: faker.address.streetName(),
-        zagat_rating: faker.finance.amount(0, 5, 1),
-        description: faker.lorem.sentence(10),
-        latitude: faker.address.latitude(),
-        longitude: faker.address.longitude(),
-        address: faker.address.streetAddress('###'),
-        website: faker.internet.url(),
-        price_level: faker.random.number(3),
-        type: placeTypes[faker.random.number(5)]
-      }
-      data.push(restaurantRow);
-    }
-    return Promise.resolve(data);
+
+function generateReviewDataBatch(round) {
+  console.log(round);
+  if (round > 24999) {
+    return Promise.resolve(null);
   }
+  let data = [];
+  for (var x = 0; x < 400; x++) {
+    let restId = (400 * round) + x;
+    let numReviews = faker.random.number(25) + 1;
+    for (var y = 0; y < numReviews; y++) {
+      let reviewRow = {
+        review_id: (25 * restId) + y,
+        restaurant_id: restId,
+        review: faker.lorem.sentences(3,2),
+        rating: faker.random.number(5)
+      }
+      data.push(reviewRow);
+    }
+  }
+  return Promise.resolve(data);
+}
+
+db.tx('inserting-reviews', t => {
+  return t.sequence(index => {
+    return generateReviewDataBatch(index)
+    .then(data => {
+      if (data) {
+        const insert = pgp.helpers.insert(data, csReviews);
+        return t.none(insert);
+      }
+    });
+  })
+})
+  .then((data) => {
+    console.log('inserted reviews');
+  })
+  .catch(error => {
+    console.log(error);
+  })
