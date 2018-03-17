7e9e96e21ffd2c81e526a71a1b8547eeb310215e
Annisa <annisakaraca@gmail.com>
2018-03-17 15:21:59 -0700

    Fix number of batches generated so that it runs the correct number of times

--- db/seedData/seedPG.js
+++ db/seedData/seedPG.js
@@ -5,15 +5,7 @@ const faker = require('faker');
 
 const db = pgp('postgres://postgres:testpw@localhost:5432/marzagat');
 
-// db.connect()
-//   .then(obj => {
-//     console.log('connected!');
-//   })
-//   .catch(error => {
-//     console.log('ERROR: ', error.message || error);
-//   })
-
-const cs = new pgp.helpers.ColumnSet([
+const csRestaurants = new pgp.helpers.ColumnSet([
   'id',
   'name',
   'neighborhood',
@@ -27,40 +19,24 @@ const cs = new pgp.helpers.ColumnSet([
   'type'
 ], {table: 'restaurants'});
 
-// const data = [
-//   {id: 1, 
-//   name: 'test restaurant', 
-//   neighborhood: 'test neighborhood',
-//   zagat_rating: 3.4,
-//   description: 'test description',
-//   latitude: 65.2341,
-//   longitude: 76.1234,
-//   address: 'test address',
-//   website: 'test website',
-//   price_level: 2,
-//   type: 'test type' 
-//   },
-//   {id: 2, 
-//     name: 'test restaurant2', 
-//     neighborhood: 'test neighborhood2',
-//     zagat_rating: 1.2,
-//     description: 'test description2',
-//     latitude: 65.2341,
-//     longitude: 76.1234,
-//     address: 'test address2',
-//     website: 'test website2',
-//     price_level: 1,
-//     type: 'test type2' 
-//     },
-// ];
+const csReviews = new pgp.helpersColumnSet([
+  'review_id',
+  'restaurant_id',
+  'review',
+  'rating'
+])
 
 const placeTypes = ['bakery', 'bar', 'cafe', 'restaurant', 'meal_delivery', 'meal_takeaway'];
 
 function generateRestaurantDataBatch(round) {
+  console.log(round);
+  if (round > 1000) {
+    return Promise.resolve(null);
+  }
   let data = [];
   for (var x = 0; x < 10000; x++) {
     let restaurantRow = {
-      id: (1000 * round) + x,
+      id: (10000 * round) + x,
       name: faker.company.companyName(), 
       neighborhood: faker.address.streetName(),
       zagat_rating: faker.finance.amount(0, 5, 1),
@@ -77,15 +53,16 @@ function generateRestaurantDataBatch(round) {
   return Promise.resolve(data);
 }
 
-db.task('inserting-products', t => {
-  return generateRestaurantDataBatch(0)
+db.tx('inserting-products', t => {
+  return t.sequence(index => {
+    return generateRestaurantDataBatch(index)
     .then(data => {
-      console.log(data.length);
       if (data) {
-        const insert = pgp.helpers.insert(data, cs);
+        const insert = pgp.helpers.insert(data, csRestaurants);
         return t.none(insert);
       }
     });
+  })
 })
   .then((data) => {
     console.log('inserted batch');
@@ -93,3 +70,29 @@ db.task('inserting-products', t => {
   .catch(error => {
     console.log(error);
   })
+
+  function generateReviewDataBatch(round) {
+    console.log(round);
+    if (round > 25000) {
+      return Promise.resolve(null);
+    }
+    let data = [];
+    for (var x = 0; x < 400; x++) {
+      let restaurantRow = {
+        review_id: (400*round) + x,
+        restaurant_id: (400*round) + x
+        name: faker.company.companyName(), 
+        neighborhood: faker.address.streetName(),
+        zagat_rating: faker.finance.amount(0, 5, 1),
+        description: faker.lorem.sentence(10),
+        latitude: faker.address.latitude(),
+        longitude: faker.address.longitude(),
+        address: faker.address.streetAddress('###'),
+        website: faker.internet.url(),
+        price_level: faker.random.number(3),
+        type: placeTypes[faker.random.number(5)]
+      }
+      data.push(restaurantRow);
+    }
+    return Promise.resolve(data);
+  }
