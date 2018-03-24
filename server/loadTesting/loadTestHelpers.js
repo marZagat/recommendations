aa5dc94936a1dbe93ad4cc1a3cba968bc302458b
Annisa <annisakaraca@gmail.com>
2018-03-24 14:43:50 -0700

    Refactor popular id generator to return 1 of 10k ids

--- server/loadTesting/loadTestHelpers.js
+++ server/loadTesting/loadTestHelpers.js
@@ -4,35 +4,17 @@ module.exports = {
 }
 
 function getPopularId(requestParams, context, ee, next) {
-  let num = (Math.floor(Math.random() * 1000000))*10
-  let endDigit = Math.random() < 0.5 ? 5 : 9;
-  num += endDigit;
+  // get number that is divisible by 1000
+  let num = (Math.floor(Math.random() * 10000))*1000
   context.vars.id = num;
   return next();
 }
 
 function getNormalId(requestParams, context, ee, next) {
-  let num = (Math.floor(Math.random() * 1000000))*10
-  let random = Math.random();
-  let endDigit;
-  if (random < 0.125) {
-    endDigit = 0;
-  } else if (random < 0.25) {
-    endDigit = 1;
-  } else if (random < 0.375) {
-    endDigit = 2;
-  } else if (random < .5) {
-    endDigit = 3;
-  } else if (random < .625) {
-    endDigit = 4;
-  } else if (random < .75) {
-    endDigit = 6;
-  } else if (random < .875) {
-    endDigit = 7;
-  } else {
-    endDigit = 8;
+  let num = (Math.floor(Math.random() * 10000000));
+  if (num % 1000 === 0) {
+    num++
   }
-  num += endDigit;
   context.vars.id = num;
   return next();
 }
