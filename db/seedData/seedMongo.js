812682cbe21031d14828099cd57425dd701a7d16
Annisa <annisakaraca@gmail.com>
2018-03-30 16:47:27 -0700

    Implement changes made for seeding Docker container volume

--- db/seedData/seedMongo.js
+++ db/seedData/seedMongo.js
@@ -3,18 +3,21 @@ require('dotenv').config();
 const generateBatch = require('./generateBatch');
 const MongoClient = require('mongodb').MongoClient;
 
-const dbHost = process.env.MONGO_ADDRESS;
+const dbHost = process.env.MONGO_ADDRESS || 'localhost';
+console.log(dbHost);
 const url = `mongodb://${dbHost}:27017`;
-const dbName = process.env.MONGO_DB_NAME;
+const dbName = process.env.MONGO_DB_NAME || 'marzagat';
+
 
 MongoClient.connect(url, function(err, client) {
+  if (err) console.log(err);
   console.log('connected successfully to server');
 
   const db = client.db(dbName);
   db.collection('restaurants').drop();
 
   async function seedDB() {
-    for (var x = 0; x < 100; x++) {
+    for (var x = 0; x < 10; x++) {
       await db.collection('restaurants').insertMany(generateBatch(x));
       console.log('inserted 100k');
     }
